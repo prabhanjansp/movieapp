cb104609e4685a0321f3c8b015d6659496906976
prabhanjansp <puranikchetan97@gmail.com>
2025-05-06 22:48:25 +0530

    helmet removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
 // src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import "./style/global.css"
-import { HelmetProvider } from 'react-helmet-async';
-import { AppProvider } from './contexts/AppContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./style/global.css";
+import { AppProvider } from "./contexts/AppContext";
 // import { AppProvider } from './contexts/AppContext';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </HelmetProvider>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
